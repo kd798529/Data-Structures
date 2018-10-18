da023e4b390a279df0d2504738e934fbcee89bcb
Kwaku Dapaah <kwa2the4@gmail.com>
2018-10-18 14:45:14 -0400

    created stuff

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,13 +23,51 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    # create a new node
+    new_node = Node(value, None)
+      
+    # check if the list is empty. If the list is empty set the head and the tail to the newly created node
+    if not self.head:
+      self.head = new_node
+      self.tail = new_node
+    # if the list is not empty, set the pointer to the next node of the tail node to the newly created node. then set the tail to the newly created node
+    else:
+      self.tail.set_next(new_node)
+      self.tail = new_node
 
   def remove_head(self):
-    pass
+    if not self.head:
+      return None
+    if not self.head.get_next():
+      head = self.head
+      self.head = None
+      self.tail = None
+      return head.get_value()
+    value =  self.head.get_value()
+    self.head = self.head.get_next()
+    return value
+
 
   def contains(self, value):
-    pass
+    if not self.head:
+      return False
+    if self.head.get_value() == value:
+      return True
+    current = self.head.get_next()
+    while current:
+      if current.get_value() == value:
+        return True
+      current = current.get_next()
+
+    
 
   def get_max(self):
-    pass
+    if not self.head:
+      return None
+    maximum = self.head.get_value()
+    current = self.head.get_next()
+    while current:
+      if current.get_value() > maximum:
+        maximum = current.get_value()
+      current = current.get_next()
+    return maximum
